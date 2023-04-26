3b65287811b987c7dfb7f1d9b5d98daaa6269e43
WyattW0569 <wywal7668@gmail.com>
2023-04-26 12:05:51 -0700

    simple backend stuff

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,23 @@
 use Harmony::Testing;
+use actix_web::{get,web,App,HttpResponse,HttpServer,Responder};
 
-fn main() {
-    println!("Hello, world! From the Backend! Today's Random Number - {}", Testing::new().number );
+
+#[get("/")]
+async fn hello() -> impl Responder{
+    HttpResponse::Ok().body(format!("Your number is : {}",Testing::new().number))
+}
+
+#[get("/tom")]
+async fn tom() -> impl Responder{
+    HttpResponse::Ok().body(format!("Welcome to the tom page!!"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| App::new()
+        .service(hello)
+        .service(tom))
+        .bind("0.0.0.0:5050")?
+        .run()
+        .await 
 }
