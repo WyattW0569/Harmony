074eb428e2efaf9f86ffa28b70ccf3b38af2977c
Tom Schuster <tom@Toms-MacBook-Air.local>
2023-05-09 23:10:25 -0700

    WebSocketTest

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,29 @@
+use actix::Actor;
 use actix_files::Files;
 use actix_web::{App,HttpServer};
 
-mod api;
+mod route;
+use route::start_connection;
+mod lobby;
+use lobby::Lobby;
+mod ws;
+mod messages;
 
-use api::route::{
+use route::{
     tom,
 };
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| App::new()
-        .service(tom)
-        .service(Files::new("/", "././frontend/dist/").index_file("index.html")))
-        .bind("0.0.0.0:5050")?
+    let chat_server = Lobby::default().start();
+    HttpServer::new(move || {
+        App::new()
+            .service(tom)
+            .service(start_connection)
+            .service(Files::new("/", "././frontend/dist/").index_file("index.html"))
+            .data(chat_server.clone())
+        })
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await 
 }
